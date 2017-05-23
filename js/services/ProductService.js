46fefc62de0fa2a4b5e0d3632315cd2577d2c3f8
nickFalcone <nfalcone@u.rochester.edu>
2017-05-23 16:52:59 -0400

    search component controller and template

--- js/services/ProductService.js
+++ js/services/ProductService.js
@@ -1,16 +1,22 @@
 module.exports = {
-    name: 'ProductService',
-    func: function ($http) {
-        let items = [];
+  name: 'ProductService',
+  func: function ($http) {
+    let items = [];
 
-        return {
-            getItems: function () {
+    return {
+      getItems: function () {
 
-            },
-            search: function () {
+      },
+      search: function () {
+        $http.get('https://shop/search?q=' + product_name + '/').then(function (response) {
+          console.log(response.data);
+//          friend.username = response.data.login;
+//          friend.pic = response.data.avatar_url;
+        });
 
-            },
-        }
-        
-    },
+
+      },
+    }
+
+  },
 }
